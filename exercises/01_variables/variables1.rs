6c7d3f8b1966bf15414350871bb5285aa17e0c1c
algoVicKe <premiumdesigns22@gmail.com>
2026-07-09 22:19:54 +0300

    Delete exercises/01_variables directory

--- exercises/01_variables/variables1.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    // TODO: Add the missing keyword.
-    let x = 5;
-
-    println!("x has the value {x}");
-    println!("This is the second exercise and it has taught me about variable kewords");
-}
