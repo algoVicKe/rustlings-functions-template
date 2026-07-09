4d967f2956049ee4a77b9b4a93e3984b4342fb6b
AlgoVicKe <premiumdesigns22@gmail.com>
2026-07-09 21:26:35 +0300

    Fix variable-based errors
    Solve all the variable-based compilation errors
    in the files under the exercises/01_variables
    folder in the rustlings directory.

---
+++ exercises/01_variables/variables1.rs
@@ -0,0 +1,7 @@
+fn main() {
+    // TODO: Add the missing keyword.
+    let x = 5;
+
+    println!("x has the value {x}");
+    println!("This is the second exercise and it has taught me about variable kewords");
+}
---
+++ exercises/01_variables/variables6.rs
@@ -0,0 +1,7 @@
+// TODO: Change the line below to fix the compiler error.
+const NUMBER:i32 = 3;
+
+fn main() {
+    println!("Number: {NUMBER}");
+    println!("In this exercise,I have added a type specification that was missing in the variable defintion,which is against rust's rules.")
+}
